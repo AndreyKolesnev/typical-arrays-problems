d2e54f106a3b72bad5d935af084f1e5375ffbfdb
Andrey <andreyjdv@gmail.com>
2021-02-22 17:50:48 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  let min = 0;
+  for (let i = 0; i < array.lenght; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    } 
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let max = 0;
+  for (let i = 0; i < array.lenght; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    } 
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  for (let i = 0; i < array.lenght; i++) {
+    sum = sum + array[i];
+  }
+  return sum / array.lenght;
 }
